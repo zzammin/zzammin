71a0116fd7ee6805580ace5371c1b7b3883ea4c1
zzammin <woalsdl7399@naver.com>
2022-08-09 21:18:08 +0900

    first commit

--- 시뮬레이션 프로젝트.py
+++ 시뮬레이션 프로젝트.py
@@ -1,16 +1,10 @@
+# 테이블 : 다른 장소 가기, 안주 먹기, 자리에 없는사람 잔 확인(4개의 잔 중 하나만 확인 가능)
+# 편의점 : 테이블로 돌아가기, 아이스크림(1턴, 취기 - 포만도 + ), 숙취해소제(2턴, 취기 -- )
+
 import pygame
 
-# 게임 초기화
 pygame.init()
+
 screen=pygame.display.set_mode((480,640))
+pygame.display.set_caption("chokproject")
 
-pygame.display.set_caption("재민")
-
-a=True
-while a:
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            a=False
-
-
-pygame.quit()
